c93d62059a7e54d95568ac45725427b7e1ed04bc
Ankit Saha <sahaankit669@gmail.com>
2024-09-13 14:28:53 +0530

    Update layout.tsx
    add background image

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -6,7 +6,9 @@ type Props = {
 };
 const MarketingLayout = ({ children }: Props) => {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col bg-cover bg-center"
+            style={{ backgroundImage: "url('/background2.svg')" }}
+            >
             <Header />
             <main className="flex-1 flex flex-col items-center justify-center">
                 {children}
